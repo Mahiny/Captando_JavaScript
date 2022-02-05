6fdb3fa670ee7f77b50eaf29d41a46f3da3dfbd3
Mahiny <93958141+Mahiny@users.noreply.github.com>
2022-02-05 10:01:20 -0300

    Add files via upload

---
+++ calculando_meu_gasto.js
@@ -0,0 +1,14 @@
+<meta charset="UTF-8">
+
+// Resolucao:
+
+<br>
+<br>
+
+<meta charset="UTF-8">
+<script>
+    document.write("A média do valor gasto com as viagens é ") ;
+    document.write( (200 + 100 + 300 + 400) / 4 + ".");
+</script>
+
+<br>
